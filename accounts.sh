056157a4754998d96f073413ccdd05602cdd8428
Ijtaba Hussain <ijtabahussain@live.com>
2018-01-20 11:50:08 +0000

    Helper scripts for fetching accounts and transaction list

---
+++ accounts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+private="/opt/monzo/bin/private"
+
+access_token=$(cat "$private/access_token")
+account_id=$(cat   "$private/account_id")
+
+auth_header="Authorization: Bearer $access_token"
+account_header="account_id==$account_id"
+
+curl -s -H "$auth_header" "https://api.monzo.com/accounts"
